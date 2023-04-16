09fbedfe84e58da5427421e4548027937891dcfc
DouglasSerena <douglas222serena@rede.ulbra.br>
2023-04-16 15:10:06 -0300

    test: Add new testes with random number generation

--- pkg/algorithms/quicksort/quicksort_test.go
+++ pkg/algorithms/quicksort/quicksort_test.go
@@ -1,11 +1,26 @@
 package quicksort_test
 
 import (
+	"math/rand"
+	"sort"
 	"testing"
 
 	"github.com/DouglasSerena/my-code/pkg/algorithms/quicksort"
 	"github.com/stretchr/testify/assert"
 )
+
+var list []int = make([]int, 100_000_000)
+var expected []int = make([]int, 100_000_000)
+
+func TestMain(m *testing.M) {
+	for i := range list {
+		list[i] = rand.Intn(100_000_000)
+	}
+
+	copy(list, expected)
+
+	sort.Ints(expected)
+}
 
 func TestBinarySearch(t *testing.T) {
 	t.Run("Should search value in Array<int>", func(t *testing.T) {
@@ -22,5 +37,11 @@ func TestBinarySearch(t *testing.T) {
 		quicksort.Sort(list)
 
 		assert.Equal(t, []int{1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 19}, list)
+	})
+
+	t.Run("Should search value in Array<int>", func(t *testing.T) {
+		quicksort.Sort(list)
+
+		assert.Equal(t, expected, list)
 	})
 }
